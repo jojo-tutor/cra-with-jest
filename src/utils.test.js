9d8e8f9fa0c4dcd675a624d8fe7476f4125072b7
Jojo Tutor <jeshrave003@gmail.com>
2018-06-18 17:17:53 +0800

    object assign

--- src/utils.test.js
+++ src/utils.test.js
@@ -3,5 +3,10 @@ test('object assignment', () => {
     let object2 = {}
     object2 = { ...object1 }
     object1.a = 2
+    expect(object1).toEqual({ a: 2, b: 1 })
     expect(object2).toEqual({ a: 1, b: 1 })
+})
+
+test('testing callbacks', () => {
+
 })
